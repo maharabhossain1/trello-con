78454843189a6e95c5f7aa2cc111dab38f5435e5
maharabhossain1 <maharabhossain0022@gmail.com>
2023-05-13 15:10:57 +0600

    get all boards

--- app/boards/page.js
+++ app/boards/page.js
@@ -2,19 +2,26 @@
 
 import { getBoards } from "@/api/apiService";
 import useUtility from "@/hooks/useUtilityContext";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const AllBoardsPage = () => {
   const { apiKey, apiToken, organizationId } = useUtility();
-  console.log(apiKey);
   const [boards, setBoards] = useState([]);
 
-  //   const getData = async ()=>{
-  //     try{
-  //         const res = await getBoards()
-  //     }
-  //   }
+  const getData = async () => {
+    try {
+      const res = await getBoards(organizationId, apiKey, apiToken);
+      setBoards(res.data);
+    } catch (e) {
+      console.log("error", e);
+    }
+  };
 
+  useEffect(() => {
+    if ((organizationId, apiKey, apiToken)) getData();
+  }, [organizationId, apiKey, apiToken]);
+
+  console.log("boards", boards);
   return <div>AllBoardPage</div>;
 };
 
